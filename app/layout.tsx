05ae56fdeabedb8885d8546d1a35945dac8e8157
Abhinav Kumar <abhinav.webdj@gmail.com>
2024-11-19 21:43:06 +0530

    added cloudflare to prod

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Script from "next/script";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -56,11 +57,7 @@ export const metadata: Metadata = {
     index: true,
     follow: true,
   },
-  viewport: {
-    width: 'device-width',
-    initialScale: 1,
-    maximumScale: 1,
-  },
+  
 };
 
 export default function RootLayout({
@@ -74,7 +71,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-      </body>
+      <Script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "624472ac05c742228ae8cb1e99e45156"}'></Script></body>
     </html>
   );
 }
